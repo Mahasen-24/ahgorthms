4303714b94f89e8ca97e6a85b8b9906c20ec86c0
Mahasen-24 <52667160+Mahasen-24@users.noreply.github.com>
2020-12-23 05:35:13 +0200

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,15 @@
+package manage.tree;
+
+/**
+ * here in this class we have the attributes of the node 
+ * which contains : 
+ * 1- parent , 2- right child , 3- left child , 4- color , 4- and value. 
+ * */
+public class Node {
+
+	Node parent; // parent of node
+	Node leftChild; // left child of node
+	Node rightChild; // right child of node
+	int color; // color of node
+	int value; // value of node
+}
--- TreeFrame.java
+++ TreeFrame.java
@@ -3,97 +3,139 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package GUI;
+package algorithm;
 
-import java.awt.*;
+import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.print.PrinterException;
+import static java.lang.System.out;
 import javax.swing.*;
+import javax.swing.text.JTextComponent;
+import manage.tree.RedBlackTree;
+
 
 /**
  *
- * @author pc
+ * @author hp
  */
-public class TreeFrame extends JFrame implements ActionListener{
-    JButton b1,b2,b3,b4;
-    JLabel l1;
-    JTextField t1,t2;
+
+public class TreeFrame extends JFrame {
+  
+    JButton insert_button,delete_button,find_button,clear_button,print_button;
+    JTextField insert_text,delete_text,find_text;
     JPanel p1,p2;
     
-    public TreeFrame()
-    {  //to display frame
+     
+
+
+    public TreeFrame() throws PrinterException
+    {
         this.setTitle("RedBlackTree");
         this.setVisible(true);
-        this.setSize(1500, 1500);
+        this.setSize(5000,1000);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setResizable(true);
+        this.setResizable(false);
+        
         p1=new JPanel();
         p2=new JPanel();
         p1.setBackground(Color.LIGHT_GRAY);
         p2.setBackground(Color.GRAY);
         this.setLayout(null);
-        p1.setBounds(0, 0,1500, 100);
-        p2.setBounds(0, 0,1500,1500);
-         add(p1);add(p2);
-         b1=new JButton("Insert");
-         b2=new JButton("Delete");
-         b3=new JButton("Clear");
-         b4=new JButton("Print");
-         l1=new JLabel();
-         t1=new JTextField();
-         t2=new JTextField();
-         p1.setLayout(null);
-         p2.setLayout(null);
-         t1.setBounds(10,20, 90, 25);
-         b1.setBounds(105,20, 70, 25);
-         t2.setBounds(180,20, 90, 25);
-         b2.setBounds(275,20,70, 25);
-         b3.setBounds(350,20,90, 25);
-         b4.setBounds(445,20,70,25);
-         p1.add(t1);p1.add(b1);
-         p1.add(t2);p1.add(b2);
-         p1.add(b3);
-         p1.add(b4);
-         l1.setBounds(150, 100, 1000, 500);
-         p2.add(l1);
-        b1.setBackground(Color.BLACK);
-        b1.setForeground(Color.WHITE);
-        b2.setBackground(Color.BLACK);
-        b2.setForeground(Color.WHITE);
-        b3.setBackground(Color.BLACK);
-        b3.setForeground(Color.WHITE);
-        b4.setBackground(Color.BLACK);
-        b4.setForeground(Color.WHITE);
-      //even action
-        b1.addActionListener(this);
-        b2.addActionListener(this);
+        p1.setBounds(0, 0,5000, 100);
+        p2.setBounds(0, 0,5000,700);
+        add(p1);add(p2);
         
-        
-    }
-    String node = new String(); 
-    @Override
-    public void actionPerformed(ActionEvent ae) {
+         insert_button=new JButton("Insert");
+         delete_button=new JButton("Delete");
+         find_button=new JButton("Find");
+         clear_button=new JButton("Clear");
+         print_button=new JButton("Print");
          
-        if(ae.getSource()==b1)
-        {
-            int n=1;
-            for(int i=0;i<50;i++)
-                    {
-                        node=node+" "+n;
-                        l1.setText(" "+node);
-                        n++;
-                    }
-           
-           // node=node+" "+t1.getText().toString();
-           
-            t1.setText(" ");
-
-        }
-        if(ae.getSource()==b2)
-        {
-          
-        }
-    
+         insert_text=new JTextField();
+         delete_text=new JTextField();
+         find_text=new JTextField();
+         
+         p1.setLayout(null);
+         
+         insert_text.setBounds(10,20, 90, 25);
+         insert_button.setBounds(105,20, 70, 25);
+         delete_text.setBounds(180,20, 90, 25);
+         delete_button.setBounds(275,20,70, 25);
+         find_text.setBounds(350,20,90, 25);
+         find_button.setBounds(445,20,70,25);
+         clear_button.setBounds(520,20,70,25);
+         print_button.setBounds(600,20,70,25);
+         
+         p1.add(insert_text);p1.add(insert_button);
+         p1.add(delete_text);p1.add(delete_button);
+         p1.add(find_text);p1.add(find_button);
+         p1.add(clear_button);
+         p1.add(print_button);
+         
+        insert_button.setBackground(Color.BLACK);
+        insert_button.setForeground(Color.WHITE);
+        
+        delete_button.setBackground(Color.BLACK);
+        delete_button.setForeground(Color.WHITE);
+        
+        find_button.setBackground(Color.BLACK);
+        find_button.setForeground(Color.WHITE);
+        
+        clear_button.setBackground(Color.BLACK);
+        clear_button.setForeground(Color.WHITE);
+        
+        print_button.setBackground(Color.BLACK);
+        print_button.setForeground(Color.WHITE);
+       
+        actions action=new actions();
+        
+        insert_button.addActionListener(action);
+        delete_button.addActionListener(action);
+        find_button.addActionListener(action);
+        clear_button.addActionListener(action);
+        print_button.addActionListener(action);
+        
     }
-    
+    public class actions implements ActionListener
+    {
+        RedBlackTree main_tree;
+
+        @Override
+        public void actionPerformed(ActionEvent ae) 
+        {
+            if(ae.getSource()==insert_button)
+            {
+                String inserted=insert_text.getText();
+                insert_text.setText(" ");
+                int num_inserted=Integer.parseInt(inserted);
+                main_tree.insert(num_inserted);
+            }
+            else if(ae.getSource()==delete_button)
+            {
+                String deleted=delete_text.getText();
+                delete_text.setText(" ");
+                int num_deleted=Integer.parseInt(deleted);
+                main_tree.delete(num_deleted);
+            }
+            else if(ae.getSource()==find_button)
+            {
+                String found=find_text.getText();
+                find_text.setText(" ");
+                int num_found=Integer.parseInt(found);
+                main_tree.searchTree(num_found);
+            }
+            else if(ae.getSource()==clear_button)
+            {
+                main_tree.clear();
+            }
+            else if(ae.getSource()==print_button)
+            {
+                main_tree.print();
+            }
+            
+        }
+        
+    }
 }
